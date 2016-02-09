005a26b16511cfd43920741f950653b163d4b73c
r11r <r11r@users.noreply.github.com>
2016-02-08 18:08:14 -0800

    little cleanup

--- azas/old-council.component.js
+++
@@ -1,9 +0,0 @@
-(function(azas) {
-    azas.CouncilComponent = ng.core.Component({
-        selector: '.azascouncil',
-        inputs: ['token'],
-        templateUrl: 'azas/council.component.html'
-    }).Class({
-        constructor: function() {}
-    });
-})(window.azas || (window.azas = {}));
