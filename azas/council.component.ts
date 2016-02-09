2ed4e9bb5c5aa03e254a55378e65da1f4a0802b5
r11r <r11r@users.noreply.github.com>
2016-02-08 18:02:53 -0800

    participants netcode done, UI fields missing

--- azas/council.component.ts
+++ azas/council.component.ts
@@ -1,18 +1,59 @@
 import {Component, Input, Output} from 'angular2/core';
 import {ParticipantInfo, Participant, Mascot, CouncilInfo, Council} from './model'
 import {MascotComponent} from './mascot.component'
+import {ParticipantComponent} from './participant.component'
 
 @Component({
     selector: '.azascouncil',
-    directives: [MascotComponent],    
+    directives: [MascotComponent, ParticipantComponent],    
     templateUrl: 'azas/council.component.html'
 })
 export class CouncilComponent {
 
     @Input() council: Council;
-    
+
+    public delParticipant(part: Participant) {
+        var index = this.council.participants.indexOf(part, 0);
+        if (index != undefined) {
+            this.council.participants.splice(index, 1);
+        } else {
+            console.log("ERROR: participant to be deleted not found!")
+        }
+    }
+
+    public newParticipant() {
+        this.council.participants.push({
+            id: "",
+            councilId: this.council.info.id,
+            priority: this.council.participants.length,
+            approved: false,
+            info: {
+                firstName: "",
+                lastName: "",
+                nickName: "",
+                email: "",
+                cell: "",
+                gremium: "",
+                tshirt: "",
+                robe: "",
+                food: "",
+                allergies: "",
+                excursion1: "",
+                excursion2: "",
+                excursion3: "",
+                dayOfBirth: "",
+                nationality: "",
+                address: "",
+                comment: "",
+                zaepfchen: false,
+                swimmer: "",
+                snorer: "",
+                arrival: ""
+            }
+        });
+    }
+
     public delMascot(mascot: Mascot) {
-        console.log("delMascot event triggered")
         var index = this.council.mascots.indexOf(mascot, 0);
         if (index != undefined) {
             this.council.mascots.splice(index, 1);
---
+++ azas/participant.component.ts
@@ -0,0 +1,71 @@
+import {Component, Input, Output, EventEmitter, OnInit} from 'angular2/core';
+import {Identifiable, Participant, ParticipantInfo} from './model'
+import {RestClient} from './rest'
+
+@Component({
+	selector: '.azasparticipant',
+	templateUrl: 'azas/participant.component.html'
+})
+export class ParticipantComponent implements OnInit {
+
+    @Input() public token: string;
+    @Input() public part: Participant;
+    @Output() public deleted: EventEmitter<Participant> = new EventEmitter();
+    public state = 'display';
+
+    constructor(private rest: RestClient) {}    
+    
+    public ngOnInit() {
+        if(this.part.id=="") {
+            this.state = 'edit'
+        }
+    }
+
+    public edit() {
+        this.state = 'edit';
+    }
+
+    public save() {
+        this.state = 'save';
+        if(this.part.id=="") this.rest.addParticipant(
+            this.token,
+            this.part.info,
+            (response) => {
+            this.part.id = (<Identifiable> response).id;
+                this.state = 'display';
+            },
+            (code) => {
+                window.alert("Serverfehler: "+code.toString());
+            }
+        );
+        else this.rest.editParticipant(
+            this.token,
+            this.part.id,
+            this.part.priority,
+            this.part.info,
+            (response) => {
+                this.state = 'display';
+            },
+            (code) => {
+                window.alert("Serverfehler: "+code.toString());
+            }
+        );
+    }
+
+    public delete() {
+        if(!window.confirm(this.part.info.nickName + " wirklich löschen :-(")) return;
+        this.state = 'delete';
+        if(this.part.id=="") this.deleted.emit(this.part);
+        else this.rest.deleteParticipant(
+            this.token,
+            this.part.id,
+            (response) => {
+                this.deleted.emit(this.part);
+            },
+            (code) => {
+                window.alert("Serverfehler: "+code.toString());
+            }
+        );
+    }
+    
+}
